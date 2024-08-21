cec30d8a9ba2099a3f1de3a729a31564395dc188
Abhinav Sonawane <abhinav.sonawane24@sanjivani.edu.in>
2024-08-21 08:10:53 +0530

    Code of string(concatenation)
    ●This is a program for string ( concatenation )
    1.We are joining a two words which is first abhinav & second is.  
        sonawane with the help of string ( concatenation).
    2.result:(we got result as)
     Concantenated: abhinav sonawane

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+
+{
+  string str1 ="abhinav";
+  string str2 ="sonawane";
+  string result=str1+" "+str2;
+  cout<<"concantenated:"<<result<<endl;
+  
+  return 0;
+}

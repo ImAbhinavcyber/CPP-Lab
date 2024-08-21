aaf8e063b8ae0b20f6b1c93609162124078c85b1
Abhinav Sonawane <abhinav.sonawane24@sanjivani.edu.in>
2024-08-21 15:21:18 +0530

    Codecfor String(substring Extraction)
    ●Extract a portion of string based on starting position and length
    ●Extracting "sonawane" from "abhinav sonawane "

---
+++ Main (1).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+
+{
+  string text="abhinav sonawane";
+  string sub=text.substr(8,8);
+  
+  cout<<"substring. "<<sub<<endl;
+  
+  return 0;
+}

7fcd20ac0d32ed44e04b9703c10bc2ff2ca7855f
Abhinav Sonawane <abhinav.sonawane24@sanjivani.edu.in>
2024-11-16 15:25:19 +0530

    Library function

---
+++ Main (3).cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <cmath>  // Include cmath for math functions 
+using  namespace  std ; 
+int  main () { 
+double  num =  16.0 ; 
+double  base =  2.0 , exponent =  3.0 ; 
+// Using sqrt() function to find the square root 
+cout  <<  "Square root of "  << num <<  " is "  <<  sqrt (num) << 
+endl ; 
+// Using pow() function to calculate power 
+cout  << base <<  " raised to the power "  << exponent  <<  " is " 
+<<  pow (base, exponent) <<  endl ; 
+return  0 ; 
+}
